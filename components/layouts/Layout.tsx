5499b184c28f64f936de360ce2bc1a205c6cc734
Jesus Alexis <ingjesusalexis@gmail.com>
2023-04-20 12:01:00 -0400

    added api-pokemons

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from 'react'
 import Head from 'next/head'
+import { Navbar } from '../ui'
 
 interface Props extends PropsWithChildren {
   title?: string
@@ -14,7 +15,8 @@ export const Layout = ({ children, title }: Props) => {
         <meta name="description" content="Informacion sobre el pokemon:" />
         <meta name="keywords" content="pokemon, pokedex, nombre del pokemon" />
       </Head>
-      <main>{children}</main>
+      <Navbar />
+      <main style={{ padding: '0px 20px' }}>{children}</main>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,46 @@
 import { Button } from '@nextui-org/react'
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
 import { Layout } from '../../components/layouts'
+import { pokeApi } from '../../api'
+import { PokemonListResponse, SmallPokemon } from '../../interfaces/'
 
-const HomePage: NextPage = () => {
+interface Props {
+  pokemons: SmallPokemon[]
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
     <>
-      <Layout title={'Pikachu'}>
-        <Button color="primary">Aceptar</Button>
+      <Layout title="Listado de Pokemons">
+        <ul>
+          {pokemons?.map((poke) => {
+            return (
+              <li key={poke.id}>
+                {poke.id} {poke.name}
+              </li>
+            )
+          })}
+        </ul>
       </Layout>
     </>
   )
 }
 export default HomePage
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
+
+  const pokemons: SmallPokemon[] = data.results.map((res, index) => ({
+    ...res,
+    id: index + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+      index + 1
+    }.png`
+  }))
+
+  return {
+    props: {
+      pokemons
+    }
+  }
+}
