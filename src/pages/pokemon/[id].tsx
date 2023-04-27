d79d4987c180b99af2d5b6e522d0e6d6e865adf6
Jesus Alexis <ingjesusalexis@gmail.com>
2023-04-27 10:14:56 -0400

    added getStaticPaths

--- src/pages/pokemon/[id].tsx
+++ src/pages/pokemon/[id].tsx
@@ -1,9 +1,43 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { SmallPokemon } from '../../../interfaces'
 
-const PokemonPage = () => {
+interface Props {
+  id: string
+  name: string
+}
+
+const PokemonPage: NextPage<Props> = ({ id, name }) => {
   const router = useRouter()
   console.log(router.query)
 
   return <div>PokemonPage</div>
 }
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  return {
+    paths: [
+      {
+        params: { id: '1' }
+      },
+      {
+        params: { id: '2' }
+      },
+      {
+        params: { id: '3' }
+      }
+    ],
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  return {
+    props: {
+      id: 1,
+      name: 'Bulbasor'
+    }
+  }
+}
+
 export default PokemonPage
